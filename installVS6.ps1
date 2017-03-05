8a219a7e713be95c16ab4102c131e57f33c4ff64
gleejs <gleejs@Gregorys-iMac.local>
2017-03-05 12:05:26 +0800

    5/3/2016 Add install VS6.ps1

---
+++ installVS6.ps1
@@ -0,0 +1,12 @@
+$srvName="ServerName"
+netsh advfirewall set allprofiles state off
+$instDIR="\\$srvName\Deploymentshare$\Applications\VS6.0\Setup"
+$dotProg="$instDIR\Setup\Acmsetup.exe"
+$dotArgs=" /T $INSTDIR\Setup\Exact.stf /S $INSTDIR /n'Exact' /o'Exact' /qn1"
+regedit /S $instDIR\key.dat
+start-process $dotProg "$dotArgs" -wait
+$instDIR="\\$srvName\Deploymentshare$\Applications\VS6.0"
+$dotProg="$instDIR\sp6\acmsetup.exe"
+$dotARGS=" /T $INSTDIR\sp6\sp698ent.stf /qn1"
+start-process $dotProg "$dotArgs" -wait
+New-ItemProperty -Path HKLM:Software\Microsoft\Windows\CurrentVersion\policies\system -Name EnableLUA -PropertyType DWord -Value 0 -Force
